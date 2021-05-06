ff279522f28dbaa0cc554df22c4cf029c400cf3f
addy024 <phulallwaraditya1999@gmail.com>
2021-05-06 10:25:07 +0530

    final push

---
+++ dicestimulator.py
@@ -0,0 +1,52 @@
+import random
+
+print("DICE STIMULATOR!!!")
+
+play = "y"
+
+while play == "y":
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("-----------")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("-----------") 
+    
+    if number == 2:
+        print("-----------")
+        print("|         |")
+        print("| 0     0 |")
+        print("|         |")
+        print("-----------")
+
+    if number == 3:
+        print("-----------")
+        print("|0        |") 
+        print("|    0    |") 
+        print("|        0|")
+        print("-----------")
+
+    if number == 4:
+        print("-----------")  
+        print("| 0     0 |")
+        print("|         |")
+        print("| 0     0 |")
+        print("-----------")
+
+    if number == 5:
+        print("-----------")
+        print("| 0     0 |")
+        print("|    0    |")
+        print("| 0     0 |")
+        print("-----------")   
+
+    if number == 6:
+        print("-----------") 
+        print("| 0     0 |") 
+        print("| 0     0 |") 
+        print("| 0     0 |")
+        print("-----------") 
+
+    play = input("Press y to roll again or any other number to exit!").lower()
